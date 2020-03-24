bd6df65c0e69a3b85010282ec6476900efeba37a
Seybel <seyibello31@gmail.com>
2020-03-24 19:20:07 +0100

    Added data files

---
+++ src/views/Dogs.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <b-table striped hover :items="items"></b-table>
+    </div>
+</template>
+
+<script>
+import dogs from '@/data/dogs.js'
+export default {
+  data () {
+    return {
+      dogs
+    }
+  }
+}
+</script>
